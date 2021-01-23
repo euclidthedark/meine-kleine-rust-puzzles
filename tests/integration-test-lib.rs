ea990ebe4e5bf2575383f6a99b5e4ef888e03db7
euclidthedark <euclidthedark@gmail.com>
2021-01-22 21:15:08 -0500

    change array values in two_number_sum_with_hash_map_test

--- tests/integration-test-lib.rs
+++ tests/integration-test-lib.rs
@@ -3,7 +3,7 @@ use puzzles;
 #[test]
 fn test_two_number_sum_with_hash_map () {
     let list_1 = [1, 4, -1, 12, 111, 44];
-    let list_2 = [-2, -4, -4, 5, 77, 98];
+    let list_2 = [-4, -4, 3, 5, 77, 98];
 
     assert_eq!(puzzles::two_number_sum_with_hash_map(&list_1, 11), [-1, 12], "Addends do not add to sum {}.", 11);
     assert_eq!(puzzles::two_number_sum_with_hash_map(&list_2, -8), [-4, -4], "Addends do not add to sum {}.", -8)
